1dafa8c86994f8555ec9084286d13be480825966
MrGramps91 <texas9137@gmail.com>
2019-05-25 09:12:08 -0400

    Add files via upload

---
+++ Chip8Emu/Chip8Emu.cpp
@@ -0,0 +1,39 @@
+// Chip8Emu.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include "GL/glut.h" //Graphics and input
+#include "Chip8.h" //Cpu core implementation
+#include "Opcodes.h" //CPU opcode implementations
+
+
+Chip8 emuChip8;
+
+
+int main(int argc,char **argv)
+{
+	//set up render sys and register input callbacks
+	setUpGraphics();
+	setUpInput();
+
+	//Initialize the Chip8 sys and load game into the memory
+	emuChip8.initialize();
+	emuChip8.loadGame("insert game name");
+
+	//Emulation loop
+	for (;;)
+	{
+		//Emulate one cycle
+		emuChip8.emulateCycle();
+		
+		//if the draw flag is set update screen
+		if (emuChip8.drawFlag)
+			drawGraphics();
+
+		//Store key press state (Press and Release)
+		emuChip8.setKeys();
+	}
+	return 0;
+
+}
+
+
---
+++ Chip8Emu/Opcodes.h
@@ -0,0 +1,26 @@
+class OpCodes {
+private:
+	unsigned short Call;
+	unsigned short Display;
+	unsigned short Flow;
+	unsigned short Flow;
+	unsigned short Cond;
+	unsigned short Cond;
+	unsigned short Cond;
+	unsigned short MEM;
+	unsigned short Flow;
+	unsigned short Rand;
+	unsigned short Disp;
+	unsigned short KeyOp;
+	unsigned short KeyOp;
+	unsigned short Timer;
+	unsigned short KeyOp;
+	unsigned short Timer;
+	unsigned short Sound;
+	unsigned short MEM;
+	unsigned short MEM;
+	unsigned short BCD;
+	unsigned short MEM;
+	unsigned short MEM;
+};
+#pragma once
